b631d891a356419a229af4a01ef92faa9f8dc47c
Daesol <55471246+Daesol@users.noreply.github.com>
2025-05-29 02:58:38 -0400

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -79,7 +79,6 @@ function AppContent() {
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(auth)" />
         <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="index" options={{ href: null }} />
       </Stack>
       <StatusBar style="light" />
     </>
